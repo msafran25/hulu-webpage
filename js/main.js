44c31c4c1fb2876396d120e017775ac7d8d7f5de
msafran25 <msafran25@gmail.com>
2021-08-03 11:06:08 +0400

    hulu-webapp

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector('.modal')
+const loginBtn = document.querySelector('.login-btn')
+const closeBtn = document.querySelector('.close')
+
+loginBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
+window.addEventListener('click', outsideClick)
+
+function openModal() {
+  modal.style.display = 'block'
+}
+
+function closeModal() {
+  modal.style.display = 'none'
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    closeModal()
+  }
+}
+
+const date = new Date();
+const year = document.getElementById('year');
+let val;
+val = date.getFullYear();
+year.textContent = `${val}`
